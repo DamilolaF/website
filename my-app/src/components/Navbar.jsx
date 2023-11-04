89074e3a01b23bf360cc88f9a0446d2a8a65daaf
Oluwadamilola Fagbore <39337392+DamilolaF@users.noreply.github.com>
2023-11-04 13:58:33 +0100

    update commit

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import Logo from '../images/logo.png'
 import {links} from '../data'
 import {FaBars} from 'react-icons/fa6'
+import {MdOutlineClose} from 'react-icons/md'
 import './navbar.css'
 
 
 
 const Navbar = () => {
+  const[isNavShowing, setIsNavShowing] = useState(false);
+
   return (
     <nav>
         <div className="container nav_container">
            <Link to='/' className='logo'>
             <img src={Logo} alt="Nav Logo" />
            </Link>
-           <ul className='nav_links'>
+           <ul className={`nav_links ${isNavShowing ? 'show_nav':'hide_nav'}`}>
                 {
                     links.map(({name, path}, index) => {
                      return (
@@ -24,8 +28,11 @@ const Navbar = () => {
                     })
                 }
            </ul>
-           <button className="nav_toggle-btn">
-            <FaBars />
+           <button className="nav_toggle-btn" onClick={() => setIsNavShowing
+            (!isNavShowing)}>
+              {
+                isNavShowing ? <MdOutlineClose/> : <FaBars/>
+              }
            </button>
         </div>
     </nav>
